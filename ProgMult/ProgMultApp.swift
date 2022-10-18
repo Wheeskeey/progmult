49057eac73199e7af7e2db0315fcf71e905297c9
Michał Sadurski <mcplay7@gmail.com>
2022-10-18 12:07:41 +0200

    Initial Commit

---
+++ ProgMult/ProgMultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgMultApp.swift
+//  ProgMult
+//
+//  Created by Michał Sadurski on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProgMultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
